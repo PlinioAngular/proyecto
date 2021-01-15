abf0aeb5ce02455dad8a5efc4dfaf4a89dc8b8cd
PlinioAngular <47014509+PlinioAngular@users.noreply.github.com>
2021-01-15 11:08:14 -0500

    First Commit

---
+++ src/main/java/com/proyecto/web/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.proyecto.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("holas")
+public class HelloWorldController {
+	
+	@GetMapping
+	public String saludo() {
+		return "Hola mundo";
+	}
+
+}
